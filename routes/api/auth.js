b6b7638839c6572863fe1b7441f03feda48c7181
MagdaLiskiewicz <156846346+MagdaLiskiewicz@users.noreply.github.com>
2025-02-05 14:10:44 +0100

    email

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,7 @@ const {
 } = require("../../controllers/authController");
 const authMiddleware = require("../../middlewares/auth");
 const upload = require("../../middlewares/upload");
+const { verifyEmail, resendVerificationEmail } = require("../../email/verify");
 
 const router = express.Router();
 
@@ -20,5 +21,9 @@ router.post("/logout", authMiddleware, logout);
 router.get("/current", authMiddleware, getCurrentUser);
 
 router.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar);
+
+router.get("/verify/:verificationToken", verifyEmail);
+
+router.post("/verify", resendVerificationEmail);
 
 module.exports = router;
